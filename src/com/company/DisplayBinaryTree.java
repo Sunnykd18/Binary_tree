30cd95b692961977e35e2fd26fa967fb1a3d97bd
Sunnykd18 <95135405+Sunnykd18@users.noreply.github.com>
2022-01-09 15:02:08 +0530

    Dispaly Binary Tree

--- src/com/company/DisplayBinaryTree.java
+++ src/com/company/DisplayBinaryTree.java
@@ -1,15 +1,14 @@
 package com.company;
 
-
 import java.util.Stack;
 
-public class Main {
+public class DisplayBinaryTree {
     public static class Node{
         int data;
         Node left;
         Node right;
-//        setting Node
-        Node(int data, Node left,Node right){
+
+        Node(int data, Node left, Node right){
             this.data = data;
             this.left = left;
             this.right = right;
@@ -19,33 +18,42 @@ public class Main {
         Node node;
         int state;
 
-         Pair(Node node, int state){
+        Pair(Node node, int state){
             this.node = node;
             this.state = state;
-
         }
-
     }
+    public static void Display(Node node){
+        if (node == null){
+            return;
+        }
+        String str = "";
+        str += node.left == null? ".": node.left.data + "";
+        str += "<-" + node.data + "->";
+        str += node.right == null? ".": node.right.data + "";
+        System.out.println(str);
 
-    public static void main(String[] args) {
-        Integer[] arr = {50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
-        Node root = new Node(arr[0] , null, null);
-        Pair rtp = new Pair(root,1);
-
+        Display(node.left);
+        Display(node.right);
+    }
+    public static void main(String []args)throws Exception{
+        Integer [] arr = {50, 25, 12, null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
+        Node root = new Node(arr[0], null,null);
+        Pair rtp = new Pair(root, 1);
 
         Stack<Pair> st = new Stack<>();
         st.push(rtp);
 
         int idx = 0;
-        while(st.size() > 0){
+        while (st.size() > 0){
             Pair top = st.peek();
             if (top.state == 1){
                 idx++;
                 if (arr[idx] != null){
-                    top.node.left = new Node(arr[idx],null, null);
-                    Pair lp = new Pair(top.node.left, 1);
+                    top.node.left = new Node(arr[idx],null,null);
+                    Pair lp = new Pair(top.node.left,1);
                     st.push(lp);
-                }else {
+                }else{
                     top.node.left = null;
                 }
                 top.state++;
@@ -62,7 +70,8 @@ public class Main {
             }else {
                 st.pop();
             }
-
         }
+        Display(root);
     }
+
 }
